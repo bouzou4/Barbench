2c2ed16300073434d7b67081233517fc077ca0b4
Adam Bouzourene <>
2018-05-29 06:23:35 -0400

    fix up anchors in smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ function scrollEffects() {
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 			if (target.length) {
 				$('html,body').animate({
-					scrollTop: (target.offset().top + 100)
+					scrollTop: (target.offset().top - 100)
 				}, 1000);
 			return false;
 			}
